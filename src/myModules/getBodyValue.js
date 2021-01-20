339da9e0a70c66e9e2d51d776c1f5d7bec91bcc5
huggo <huggo.tricottet@hotmail.fr>
2021-01-20 14:32:14 +0100

    prout

---
+++ src/myModules/getBodyValue.js
@@ -0,0 +1,7 @@
+class BodyValue {
+    constructor(weight, tauxg, activity) {
+      this.caloDay = (370+21.6*(1-tauxg)*weight)*activity;
+    }
+  }
+const test = new BodyValue(88, 0.4, 2)
+console.log(test.caloDay)
